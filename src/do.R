2bf0f387e2237910d94144d591abb0dbba32f7d4
bmiles <ben.miles2@gmail.com>
2014-05-21 16:37:07 +0100

    initial commit

--- src/do.R
+++ src/do.R
@@ -1,5 +1,5 @@
 source('src/func.R')
-groupHooks <- c("aptes","coll","Analyst") #sample is the optics bacgrkound measurement
+groupHooks <- c("silicon","APTES","collagen") #sample is the optics bacgrkound measurement
 groupColors <- brewer.pal(length(groupHooks), "Dark2")
 
 setwd('data')
@@ -20,18 +20,31 @@ baseSpec <- baselineSpec(saveLoadReference, int=FALSE, retC=TRUE, method="rfbase
 newIR <- removeFreq(baseSpec, rem.freq =
                       baseSpec$freq > 1800 & baseSpec$freq < 2500)
 
-newFileName <- paste(getwd(), "/graphs/ATR-FTIR data for different substrates with collagen, 16-05-2014", ".pdf", sep = "")
+newFileName <- paste(getwd(), "/graphs/ATR-FTIR test with a Ge ATR crystal 21-05-2014", ".pdf", sep = "")
   pdf(file=newFileName, paper="a4r", width=9)
   par(mfrow = c(1,1), mar = c(5.1,5.1,4.1,2.1))
-  plotSpectra(baseSpec, which=c(1,2,3,4,5), title="ATR-FTIR data for different substrates with collagen, 16-05-2014", offset= 0.05, yrange=c(-0.1,0.75), xaxt = "n")
+  plotSpectra(baseSpec,
+              which=c(3,2,1),
+              xlim=c(4400,400),
+              title="ATR-FTIR test with a Ge ATR crystal, 21-05-2014",
+              offset= 0.02,
+              yrange=c(-0.01,0.25),
+              xaxt = "n")
   at <- seq(from = 0, to = max(baseSpec$freq), by = 100)
   axis(side = 1, at = at, las = 2, hadj = 0.9)
 dev.off()
 
-newFileName <- paste(getwd(), "/graphs/ATR-FTIR data for different substrates with collagen detail, 16-05-2014", ".pdf", sep = "")
+newFileName <- paste(getwd(), "/graphs/ATR-FTIR test with a Ge ATR crystal detail 21-05-2014", ".pdf", sep = "")
   pdf(file=newFileName, paper="a4r", width=9)
   par(mfrow = c(1,1), mar = c(5.1,5.1,4.1,2.1))
-  plotSpectra(baseSpec, which=c(1,2,3,4,5), xlim=c(1800,932), title="ATR-FTIR data for different substrates with collagen, 09-05-2014", offset= 0.03, yrange=c(-0.01,0.4), xaxt = "n",  lab.pos = (mean(baseSpec$freq)*0.65))
+  plotSpectra(baseSpec,
+              which=c(3,2,1),
+              xlim=c(1800,800),
+              title="ATR-FTIR test with a Ge ATR crystal APTES detail, 21-05-2014",
+              offset= 0.01,
+              yrange=c(-0.01,0.15),
+              xaxt = "n",
+              lab.pos = (mean(baseSpec$freq)*0.65))
   at <- seq(from = 0, to = max(baseSpec$freq), by = 20)
   axis(side = 1, at = at, las = 2, hadj = 0.9)
 dev.off()
--- src/func.R
+++ src/func.R
@@ -1,4 +1,56 @@
+gplotSpectra <- function(spectra, which = c(2),
+  yrange = range(spectra$data),
+  offset = 0.0, amplify = 1.0,
+  lab.pos = mean(spectra$freq), ...) {
 
+# Function to plot multiple spectra @ specified expansions & decorate
+# Part of the ChemoSpec package
+# Bryan Hanson, DePauw University, June 2009
+
+  if (missing(spectra)) stop("No spectral data provided")
+  chkSpectra(spectra)
+
+  # set up and plot the first spectrum
+
+  spectrum <- spectra$data[which[1],]*amplify
+  yseq <- seq(from= yrange[1], to=yrange[2])
+  #ggplot
+   plots <- ggplot() + geom_path(aes(x= spectra$freq, y=spectrum))
+
+     which <- which[-1] # first spectrum already plotted so remove it from the list
+     count <- 0 # get the other spectra and plot them as well
+     for(n in which) {
+       count <- count + 1
+       spectrum <- (spectra$data[n,]+(offset*count))*amplify
+       #points(spectra$freq, spectrum, type = "l", col = spectra$colors[n], ...)
+       plots + geom_path(aes(x= spectra$freq, y=spectrum, colour= spectra$colors[n]))
+       #lab.y <- spectrum[spec.index]
+       #text(lab.x, lab.y, labels = spectra$names[n], pos = 3, cex = 0.75)
+       }
+     #geom_path(aes(x= spec$freq, y=spec$data[3,]))
+
+  #qplot
+
+  # qplot(spectra$freq, spectrum, geom = "path",
+  # xlab = spectra$unit[1], ylab = spectra$unit[2],
+  # ylim = yrange, col = spectra$colors[which[1]],
+  # frame.plot = FALSE, ...)
+  # grid(ny = NA, lty = 1)
+  # lab.x <- lab.pos
+  # spec.index <- findInterval(lab.x, sort(spectra$freq))
+  # lab.y <- spectrum[spec.index]
+  # text(lab.x, lab.y, labels = spectra$names[which[1]], pos = 3, cex = 0.75)
+  #
+  # which <- which[-1] # first spectrum already plotted so remove it from the list
+  # count <- 0 # get the other spectra and plot them as well
+  # for(n in which) {
+  # 	count <- count + 1
+  # 	spectrum <- (spectra$data[n,]+(offset*count))*amplify
+  # 	points(spectra$freq, spectrum, type = "l", col = spectra$colors[n], ...)
+  # 	lab.y <- spectrum[spec.index]
+  # 	text(lab.x, lab.y, labels = spectra$names[n], pos = 3, cex = 0.75)
+  # 	}
+}
 # Heat Map, returns a heat map of intensities, takes a wavenumber or range (and calculates the mean)
 irHeatmap <- function (wavenumber) {
   # Setup these parametric variables.
